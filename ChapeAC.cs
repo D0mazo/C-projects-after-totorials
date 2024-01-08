9e7ebf583ec4859c68a647a5d0593cb460f461f8
D0mazo <145331809+D0mazo@users.noreply.github.com>
2024-01-08 18:33:09 +0200

    Add files via upload

---
+++ ChapeAC.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What shape area do you want to calculate? Press 'r' for rectangle, any other key for circle:");
+
+            string answer = Console.ReadLine();
+            float result = 0;
+
+            if (answer == "r")
+            {
+                Console.WriteLine("Please enter the height of rectangle:");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of rectangle:");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height * width;
+            }
+            else
+            {
+                Console.WriteLine("Please enter the radius of circle:");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+
+            Console.WriteLine("The result is " + result);
+            Console.ReadKey();
+        }
+    }
+}
