6d56b97779a5a9519c739b6f91af6f80c56d258b
D0mazo <145331809+D0mazo@users.noreply.github.com>
2024-01-11 16:12:53 +0200

    Add files via upload
    Make as a joke (I saw during basketball games somewhere and tried to write code for same thing)

---
+++ Relationships.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Press Enter to know in which type of relationhsip you gonna end 2024...");
+
+        Console.ReadLine(); 
+
+        string[] relationshipStatuses = { "Single", "In a Relationship", "Engaged", "Married", "It's Complicated", "Gay" };
+
+        Random random = new Random();
+        int index = random.Next(relationshipStatuses.Length);
+
+        string randomStatus = relationshipStatuses[index];
+
+        Console.WriteLine($"In the end of 2024 you will be: {randomStatus}");
+    }
+}
